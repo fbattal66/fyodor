1b17036f999ad2b22da4dee85d0fad31636129d1
fyodor12345 <Vfyodor@hotmail.com>
2025-05-15 22:41:20 +0300

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -1,3 +1,11 @@
 echo "numbeeeeeewwer"
 echo "wdwd"
 echo "numbeeesdsaeeewwer"
+echo "numbeeeeeewwer"
+echo "wdwd"
+echo "numbeeesdsaeeewwer"
+
+echo "numbeeeeeewwer"
+echo "wdwd"
+echo "numbeeesdsaeeewwer"
+
