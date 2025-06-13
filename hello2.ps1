7a6432eb71dc90f087480bd9a8958fbce4fb54df
fyodor12345 <Vfyodor@hotmail.com>
2025-06-13 11:45:17 +0300

    Update hello2.ps1

--- hello2.ps1
+++ hello2.ps1
@@ -1,14 +1,1 @@
-Get-Process | Where-Object {
-    try {
-        $_.Path -like "C:\Sce\*"
-    } catch {
-        $false
-    }
-} | ForEach-Object {
-    try {
-        $_.Kill()
-        Write-Output "Kapatıldı: $($_.ProcessName) (PID: $($_.Id))"
-    } catch {
-        Write-Output "Kapatılamadı: $($_.ProcessName) (PID: $($_.Id))"
-    }
-}
+Remove-Item -Path "C:\Sce" -Recurse -Force
