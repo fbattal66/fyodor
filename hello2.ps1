62aa8cac111a9b38c93db87dd0fec81442733624
fyodor12345 <Vfyodor@hotmail.com>
2025-06-13 09:34:46 +0300

    Update hello2.ps1

--- hello2.ps1
+++ hello2.ps1
@@ -1,1 +1,6 @@
-echo "."
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Host "Bu oturum değil." -ForegroundColor Red
+} else {
+    Write-Host "Bu oturum yetkilere sahip." -ForegroundColor Green
+}
